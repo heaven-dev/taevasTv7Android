45dae95d50ed8eaf3961d74010bc677ec2b3c94b
heaven-dev <kah@live.fi>
2021-07-13 17:53:42 +0300

    Fix to current series on archive main page.

--- app/src/main/java/et/tv7/taevastv7/activities/MainActivity.java
+++ app/src/main/java/et/tv7/taevastv7/activities/MainActivity.java
@@ -38,34 +38,15 @@ import et.tv7.taevastv7.fragments.SeriesFragment;
 import et.tv7.taevastv7.fragments.SeriesInfoFragment;
 import et.tv7.taevastv7.fragments.TvMainFragment;
 import et.tv7.taevastv7.fragments.TvPlayerFragment;
-import et.tv7.taevastv7.helpers.GuideItem;
 import et.tv7.taevastv7.helpers.Utils;
 import et.tv7.taevastv7.interfaces.ArchiveDataLoadedListener;
 import et.tv7.taevastv7.model.ArchiveViewModel;
 import et.tv7.taevastv7.model.GuideViewModel;
 
-import static et.tv7.taevastv7.helpers.Constants.BROADCAST_DATE;
-import static et.tv7.taevastv7.helpers.Constants.BROADCAST_DATE_TIME;
-import static et.tv7.taevastv7.helpers.Constants.CAPTION;
 import static et.tv7.taevastv7.helpers.Constants.DATE_INDEX;
-import static et.tv7.taevastv7.helpers.Constants.DURATION;
-import static et.tv7.taevastv7.helpers.Constants.END_DATE;
-import static et.tv7.taevastv7.helpers.Constants.END_TIME;
-import static et.tv7.taevastv7.helpers.Constants.EPISODE_NUMBER;
 import static et.tv7.taevastv7.helpers.Constants.EXIT_OVERLAY_FRAGMENT;
-import static et.tv7.taevastv7.helpers.Constants.FORMATTED_END_TIME;
-import static et.tv7.taevastv7.helpers.Constants.FORMATTED_START_TIME;
 import static et.tv7.taevastv7.helpers.Constants.GUIDE_DATA;
-import static et.tv7.taevastv7.helpers.Constants.IMAGE_PATH;
-import static et.tv7.taevastv7.helpers.Constants.IS_VISIBLE_ON_VOD;
 import static et.tv7.taevastv7.helpers.Constants.LOG_TAG;
-import static et.tv7.taevastv7.helpers.Constants.NAME;
-import static et.tv7.taevastv7.helpers.Constants.SERIES;
-import static et.tv7.taevastv7.helpers.Constants.SERIES_AND_NAME;
-import static et.tv7.taevastv7.helpers.Constants.SID;
-import static et.tv7.taevastv7.helpers.Constants.START_DATE;
-import static et.tv7.taevastv7.helpers.Constants.START_END_TIME;
-import static et.tv7.taevastv7.helpers.Constants.TIME;
 import static et.tv7.taevastv7.helpers.Constants.TV_MAIN_FRAGMENT;
 import static et.tv7.taevastv7.helpers.Constants.PROGRESS_BAR_SIZE;
 
@@ -263,7 +244,7 @@ public class MainActivity extends AppCompatActivity implements ArchiveDataLoaded
                     else {
                         this.addGuideData(guideData);
 
-                        archiveViewModel.initializeSeriesData(guideViewModel.getGuide());
+                        archiveViewModel.addGuideData(guideViewModel.getGuide(), true);
 
                         this.prepareUi();
                         Utils.toPage(TV_MAIN_FRAGMENT, this, false, false, null);
@@ -321,28 +302,7 @@ public class MainActivity extends AppCompatActivity implements ArchiveDataLoaded
         for (int i = 0; i < guideData.length(); i++) {
             JSONObject obj = guideData.getJSONObject(i);
             if (obj != null) {
-                GuideItem g = new GuideItem(
-                        Utils.getJsonStringValue(obj, TIME),
-                        Utils.getJsonStringValue(obj, END_TIME),
-                        Utils.getJsonStringValue(obj, IMAGE_PATH),
-                        Utils.getJsonStringValue(obj, CAPTION),
-                        Utils.getJsonStringValue(obj, START_END_TIME),
-                        Utils.getJsonStringValue(obj, START_DATE),
-                        Utils.getJsonStringValue(obj, END_DATE),
-                        Utils.getJsonStringValue(obj, FORMATTED_START_TIME),
-                        Utils.getJsonStringValue(obj, FORMATTED_END_TIME),
-                        Utils.getJsonStringValue(obj, BROADCAST_DATE),
-                        Utils.getJsonStringValue(obj, BROADCAST_DATE_TIME),
-                        Utils.getJsonStringValue(obj, DURATION),
-                        Utils.getJsonStringValue(obj, SERIES),
-                        Utils.getJsonStringValue(obj, NAME),
-                        Utils.getJsonIntValue(obj, SID),
-                        Utils.getJsonIntValue(obj, EPISODE_NUMBER),
-                        Utils.getJsonIntValue(obj, IS_VISIBLE_ON_VOD),
-                        Utils.getJsonStringValue(obj, SERIES_AND_NAME),
-                        Utils.isStartDateToday(Utils.getJsonStringValue(obj, TIME)));
-
-                guideViewModel.addItemToGuide(g);
+                guideViewModel.addItemToGuide(Utils.getGuideItemByJsonObj(obj));
             }
         }
     }
